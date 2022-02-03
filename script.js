9b6ca8a9330d6c17db356792980363efd15bbadd
ASHISH PRAKASH <60266415+Ashish-Prakash@users.noreply.github.com>
2022-02-04 01:46:23 +0530

    Recoding succesfull

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
 let videoElement = document.querySelector("video");
 let recordButton = document.querySelector("#record");
-let recordingstate = false;
-let mediaRecord;
+let recordingState = false;
+let mediaRecorder;
 (async function(){
     let constraint = {video: true};
     let mediaStream = await navigator.mediaDevices.getUserMedia(constraint);
     videoElement.srcObject = mediaStream;
 
-    mediaRecord = new MediaRecorder(mediaStream);
-    mediaRecord.onstart = function(){
-
+    mediaRecorder = new MediaRecorder(mediaStream);
+    console.log(mediaRecorder);
+    mediaRecorder.onstart = function(){
+        console.log("Inside on");
     };
-    mediaRecord.onstop = function(){
-
+    mediaRecorder.ondataavailable = function(e){
+        console.log("Inside on data avilable");
+        console.log(e.data);
+        let videoObject = new Blob([e.data],{ type: "video/mp4" });
+        let videoUrl = URL.createObjectURL(videoObject);
+        let aTag = document.createElement("a");
+        aTag.download = `Video${Date.now()}.mp4`;
+        aTag.href = videoUrl;
+        aTag.click();
     };
-    mediaRecord.ondataavailable = function(){
-
+    mediaRecorder.onstop = function(){
+        console.log("Inside stop");
     };
+    recordButton.addEventListener("click", function(){
+        if(recordingState){
+            mediaRecorder.stop();
+            recordButton.innerHTML = "Record Video";
+            recordingState = false;
+        }
+        else{
+            mediaRecorder.start();
+            recordButton.innerHTML = "Recording..";
+            recordingState = true;
+        }
+    });
 })();
