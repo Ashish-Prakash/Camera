a6fd1112062aa29d0838638aca25716a2fb64e43
Ashish <ashishprakash52433@gmail.com>
2022-02-04 19:58:17 +0530

    capture image

--- script.js
+++ script.js
@@ -1,20 +1,52 @@
 let videoElement = document.querySelector("video");
 let recordButton = document.querySelector("#record");
-let recordingstate = false;
-let mediaRecord;
+let captureImage = document.querySelector("#capture");
+let recordingState = false; 
+let mediaRecorder;
 (async function(){
     let constraint = {video: true};
     let mediaStream = await navigator.mediaDevices.getUserMedia(constraint);
     videoElement.srcObject = mediaStream;
 
-    mediaRecord = new MediaRecorder(mediaStream);
-    mediaRecord.onstart = function(){
-
+    mediaRecorder = new MediaRecorder(mediaStream);
+    console.log(mediaRecorder);
+    mediaRecorder.onstart = function(){
+        console.log("Inside on");
     };
-    mediaRecord.onstop = function(){
-
+    mediaRecorder.ondataavailable = function(e){
+        console.log("Inside on data avilable");
+        console.log(e.data);
+        let videoObject = new Blob([e.data],{ type: "video/mp4" });
+        let videoUrl = URL.createObjectURL(videoObject);
+        let aTag = document.createElement("a");
+        aTag.download = `Video${Date.now()}.mp4`;
+        aTag.href = videoUrl;
+        aTag.click();
     };
-    mediaRecord.ondataavailable = function(){
-
+    mediaRecorder.onstop = function(){
+        console.log("Inside stop");
     };
+    recordButton.addEventListener("click", function(){
+        if(recordingState){
+            mediaRecorder.stop();
+            recordButton.innerHTML = "Record Video";
+            recordingState = false;
+        }
+        else{
+            mediaRecorder.start();
+            recordButton.innerHTML = "Recording..";
+            recordingState = true;
+        }
+    });
+    captureImage.addEventListener("click", function(){
+        let canvas = document.querySelector("canvas");
+        canvas.width = 640;
+        canvas.height = 480;
+        let ctx = canvas.getContext("2d");
+        ctx.drawImage(videoElement,0,0);
+        let aTag = document.createElement("a");
+        aTag.download = `Photo${Date.now()}.jpg`;
+        aTag.href = canvas.toDataURL("image/jpg");
+        aTag.click();
+    });
 })();
