7ebfc3e9ea827abfc31313c7c461bfe8672bd60b
ASHISH PRAKASH <60266415+Ashish-Prakash@users.noreply.github.com>
2022-02-04 19:53:21 +0530

    image  clicked

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let videoElement = document.querySelector("video");
 let recordButton = document.querySelector("#record");
-let recordingState = false;
+let captureImage = document.querySelector("#capture");
+let recordingState = false; 
 let mediaRecorder;
 (async function(){
     let constraint = {video: true};
@@ -36,5 +37,16 @@ let mediaRecorder;
             recordButton.innerHTML = "Recording..";
             recordingState = true;
         }
+    });
+    captureImage.addEventListener("click", function(){
+        let canvas = document.querySelector("canvas");
+        canvas.width = 640;
+        canvas.height = 480;
+        let ctx = canvas.getContext("2d");
+        ctx.drawImage(videoElement,0,0);
+        let aTag = document.createElement("a");
+        aTag.download = `Photo${Date.now()}.jpg`;
+        aTag.href = canvas.toDataURL("image/jpg");
+        aTag.click();
     });
 })();
